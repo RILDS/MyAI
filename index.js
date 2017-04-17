90e1dc14c6f7a4e5c6804002074e220980f4949c
HongBo Zeng(曾泓博) <HongBo_Zeng@HTC.COM>
2017-04-17 15:41:15 +0800

    [MyAI] nodejs webhook version

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.set('port', (process.env.PORT || 5000));
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+const restService = express();
+restService.use(bodyParser.json());
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+restService.post('/hook', function (req, res) {
+
+    console.log('hook request');
+
+    try {
+        var speech = 'empty speech';
+
+        if (req.body) {
+            var requestBody = req.body;
+
+            if (requestBody.result) {
+                speech = '';
+
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
+
+                if (requestBody.result.action) {
+                    speech += 'action: ' + requestBody.result.action + ' ';
+                }
+
+                var parameters = requestBody.result.parameters;
+                if (parameters){
+                    for (var p in parameters){
+                        if(parameters.hasOwnProperty(p) ) {
+                            speech += p + ": " + parameters[p] + "; ";
+                        }
+                    }
+                }
+            }
+        }
+
+        console.log('result: ', speech);
+
+        return res.json({
+            speech: speech,
+            displayText: speech,
+            source: 'apiai-webhook-sample'
+        });
+    } catch (err) {
+        console.error("Can't process request", err);
+
+        return res.status(400).json({
+            status: {
+                code: 400,
+                errorType: err.message
+            }
+        });
+    }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+restService.listen((process.env.PORT || 5000), function () {
+    console.log("Server listening");
 });
-
-
